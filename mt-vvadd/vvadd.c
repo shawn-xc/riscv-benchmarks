4ae0e8e484ca9192b67bf1a8724c57885ca405c3
Eric Love <ericlove@berkeley.edu>
2016-03-11 13:26:09 -0800

    fix to avoid false sharing

--- mt-vvadd/vvadd.c
+++ mt-vvadd/vvadd.c
@@ -9,9 +9,11 @@
 void __attribute__((noinline)) vvadd(int coreid, int ncores, size_t n, const data_t* x, const data_t* y, data_t* z)
 {
    size_t i;
+   size_t chunk_sz = n / ncores;
+   size_t base = coreid * chunk_sz;
+   size_t max = base + chunk_sz > n ? n : base + chunk_sz;
 
-   // interleave accesses
-   for (i = coreid; i < n; i+=ncores)
+   for (i = coreid * chunk_sz; i < max; i++)
    {
       z[i] = x[i] + y[i];
    }
