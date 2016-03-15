083980c250bd4f5bb7063b3e6bf9b84d81f22646
Howard Mao <zhehao.mao@gmail.com>
2016-03-14 23:47:27 -0700

    fix mt-histo

--- mt-histo/mt_histo.c
+++ mt-histo/mt_histo.c
@@ -8,45 +8,46 @@
 #define NTHREADS 16
 
 struct histo_data {
-	int start;
-	int end;
-	int nbins;
-	int *input_data;
-	int *output_bins;
+    int start;
+    int end;
+    int nbins;
+    int *input_data;
+    int *output_bins;
 };
 
-void histo_kernel(int *in_data, int ndata, int *out_bins, int nbins)
+int temp_bins[NUM_BINS * 16];
+
+void histo_kernel(int *in_data, int ndata, int *local_bins, int *out_bins, int nbins)
 {
-	int i, oldsum;
-	int localbins[nbins];
+    int i, oldsum;
 
-	memset(localbins, 0, sizeof(int) * nbins);
+    memset(local_bins, 0, sizeof(int) * nbins);
 
-	for (i = 0; i < ndata; i++) {
-		int pos = in_data[i];
-		localbins[pos]++;
-	}
+    for (i = 0; i < ndata; i++) {
+        int pos = in_data[i];
+        local_bins[pos]++;
+    }
 
-	for (i = 0; i < nbins; i++) {
-		int *dst;
+    for (i = 0; i < nbins; i++) {
+        int *dst;
 
-		if (localbins[i] == 0)
-			continue;
+        if (local_bins[i] == 0)
+            continue;
 
-		dst = &out_bins[i];
-		asm volatile ("amoadd.w %[oldsum], %[count], (%[dst])" :
-				[oldsum] "=r" (oldsum) :
-				[count] "r" (localbins[i]),
-				[dst] "r" (dst));
-	}
+        dst = &out_bins[i];
+        asm volatile ("amoadd.w %[oldsum], %[count], (%[dst])" :
+                [oldsum] "=r" (oldsum) :
+                [count] "r" (local_bins[i]),
+                [dst] "r" (dst));
+    }
 }
 
 void printBins(int *out_bins, int nbins)
 {
-	int i;
+    int i;
 
-	for (i = 0; i < nbins; i++)
-		printf("out_bins[%d] = %d\n", i, out_bins[i]);
+    for (i = 0; i < nbins; i++)
+        printf("out_bins[%d] = %d\n", i, out_bins[i]);
 }
 
 #define min(a, b) ((a) < (b)) ? (a) : (b)
@@ -54,28 +55,24 @@ void printBins(int *out_bins, int nbins)
 
 void thread_entry(int cid, int nc)
 {
-	int local_data_size = (DATA_SIZE - 1) / nc + 1;
-	int start = cid * local_data_size;
-	int end = min(start + local_data_size, DATA_SIZE);
+    int local_data_size = (DATA_SIZE - 1) / nc + 1;
+    int start = cid * local_data_size;
+    int end = min(start + local_data_size, DATA_SIZE);
+    int ret = 0;
 
-	barrier(nc);
+    stats(
+        histo_kernel(input_data + start, end - start,
+		temp_bins + cid * NUM_BINS, output_bins, NUM_BINS);
+	barrier(nc),
+	1);
 
-	if (cid == 0)
-		setStats(1);
+#ifdef DEBUG
+    // Print out the result
+    printArray("results", NUM_BINS, output_bins);
+#endif
 
-	histo_kernel(input_data + start, end - start, output_bins, NUM_BINS);
+    // Check the results
+    ret = verify(NUM_BINS, output_bins, verify_data);
 
-	barrier(nc);
-
-	if (cid == 0) {
-		setStats(0);
-
-		// Print out the result
-		printArray("results", NUM_BINS, output_bins);
-
-		// Check the results
-		exit(verify(NUM_BINS, output_bins, verify_data));
-	} else {
-		exit(0);
-	}
+    exit(ret);
 }
